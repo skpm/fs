a83b30cc4713a046fabff1ec453e9cc5da1e75b0
Mathieu Dutour <mathieu@dutour.me>
2016-12-08 18:09:51 +0000

    first commit :tada:

---
+++ index.js
@@ -0,0 +1,54 @@
+module.exports = {
+  mkdir: function (path) {
+    var error = null
+    var result = NSFileManager.defaultManager().createDirectoryAtPath_withIntermediateDirectories_attributes_error(path, true, {}, error)
+    if (error != null) {
+      throw new Error(error)
+    }
+    return result
+  },
+
+  readFile: function (path, encoding) {
+    var error = null
+    var result = NSString.stringWithContentsOfFile_encoding_error(path, encoding || NSUTF8StringEncoding, error)
+    if (error != null) {
+      throw new Error(error)
+    }
+    return result
+  },
+
+  writeFile: function (path, data, encoding) {
+    var error = null
+    var result
+    if (data.TIFFRepresentation) {
+      var tiffData = image.TIFFRepresentation()
+      var p = NSBitmapImageRep.imageRepWithData(tiffData)
+      var data = p.representationUsingType_properties(encoding || NSPNGFileType, null)
+      data.writeToFile_atomically(path, true)
+    } else {
+      result = NSString.stringWithString(str).writeToFile_atomically_encoding_error(path, true, encoding || NSUTF8StringEncoding, error)
+    }
+    if (error != null) {
+      throw new Error(error)
+    }
+    return result
+  },
+
+  rename: function (oldPath, newPath) {
+    var error = null
+    var result = NSFileManager.defaultManager().moveItemAtPath_toPath_error(oldPath, newPath, error)
+    if (error != null) {
+      throw new Error(error)
+    }
+    return result
+  },
+
+  rmdir: function (path) {
+    var error = null
+    var result = NSFileManager.defaultManager().removeItemAtPath_error(path, error)
+    if (error != null) {
+      throw new Error(error)
+    }
+    return result
+  }
+}
